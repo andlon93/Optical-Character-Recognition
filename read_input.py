6890934d689f5b6064814837f321c5ec8b91e6a5
andlon93 <andreas.lones@gmail.com>
2016-04-18 14:04:47 +0200

    reading and converting to vectors

--- read_input.py
+++ read_input.py
@@ -1,6 +1,16 @@
 from skimage.io import imread
 import numpy as np
 #
+def kd_reduce(func,seq):
+    res = seq[0]
+    for item in seq[1:]:
+        res = func(res,item)
+    return res
+def flatten_image(image_array):
+    def flatten(a,b): return a + b
+    return kd_reduce(flatten, image_array.tolist())
+#
+#print(flatten_image(imread("chars74k-lite/chars74k-lite/"+"a"+"/"+"a"+"_"+"0"+".jpg")))
 def read_input():
 	alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 	n=70
@@ -8,7 +18,7 @@ def read_input():
 	train_solutions=[]
 	for i in range(n):
 		for letter in alphabet:
-			train_data.append(imread("chars74k-lite/chars74k-lite/"+letter+"/"+letter+"_"+str(i)+".jpg"))
+			train_data.append(flatten_image(imread("chars74k-lite/chars74k-lite/"+letter+"/"+letter+"_"+str(i)+".jpg")))
 			train_solutions.append(letter)
 	#
 	m=88
@@ -16,13 +26,14 @@ def read_input():
 	test_solutions=[]
 	for i in range(n,m):
 		for letter in alphabet:
-			test_data.append(imread("chars74k-lite/chars74k-lite/"+letter+"/"+letter+"_"+str(i)+".jpg"))
+			test_data.append(flatten_image(imread("chars74k-lite/chars74k-lite/"+letter+"/"+letter+"_"+str(i)+".jpg")))
 			test_solutions.append(letter)
 	#
-	print(len(train_data))
+	'''print(len(train_data))
 	print(len(train_solutions))
 	print(len(test_data))
-	print(len(test_solutions))
+	print(len(test_solutions))'''
 	return train_data, train_solutions, test_data, test_solutions
 if __name__ == '__main__':
 	a,b,c,d=read_input()
+	print(len(a[0]))
