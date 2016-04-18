855097e190370b6c9fb507373c377086f71bc931
andlon93 <andreas.lones@gmail.com>
2016-04-18 15:22:07 +0200

    read_input_OLE made

--- read_input.py
+++ read_input.py
@@ -43,6 +43,33 @@ def read_input():
 	print(len(test_data))
 	print(len(test_solutions))
 	return train_data, train_solutions, test_data, test_solutions
+####
+def read_input_OLE():
+	alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	#alphabet=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,17,19,20,21,2]
+	n=70
+	train_data=[]
+	train_solutions=[]
+	for i in range(n):
+		for letter in alphabet:
+			train_data.append(np.asarray(flatten_image(imread("chars74k-lite/chars74k-lite/"+letter+"/"+letter+"_"+str(i)+".jpg"))))
+			train_data[-1] = np.divide(train_data[-1],255)
+			train_solutions.append(ord(letter))
+	#
+	m=88
+	test_data=[]
+	test_solutions=[]
+	for j in range(n,m):
+		for letter in alphabet:
+			test_data.append(np.asarray(flatten_image(imread("chars74k-lite/chars74k-lite/"+letter+"/"+letter+"_"+str(j)+".jpg"))))
+			test_data[-1] = np.divide(test_data[-1],255)
+			test_solutions.append(ord(letter))
+	#
+	print(len(train_data))
+	print(len(train_solutions))
+	print(len(test_data))
+	print(test_solutions)
+	return train_data, train_solutions, test_data, test_solutions
 if __name__ == '__main__':
-	a,b,c,d=read_input()
+	a,b,c,d=read_input_OLE()
 	#print(len(a[0]))
